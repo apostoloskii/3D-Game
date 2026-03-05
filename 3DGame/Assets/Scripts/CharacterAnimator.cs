be203ea7821b4c56539a447674540094d58ab133
Leonid <7bss58fmgv@privaterelay.appleid.com>
2026-03-05 12:36:56 +0100

    Poslednoto video zavrsheno
    Gi zavrshiv site videa no imam nekolku errori ne tehnichki tuku ne mi rabotat samo animaciite na ENEMY i na PLAYER i treba samo uste da ja dodadam PLATFORMATA

---
+++ 3DGame/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    public AnimationClip replaceableAttackAnim;
+    public AnimationClip[] defaultAttackAnimSet;
+    protected AnimationClip[] currentAttackAnimSet;
+
+    const float locomationAnimationSmoothTime = .1f;
+
+    NavMeshAgent agent;
+    protected Animator animator;
+    protected CharacterCombat combat;
+    public AnimatorOverrideController overrideController;
+
+    protected virtual void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        combat = GetComponent<CharacterCombat>();
+
+        if (overrideController == null)
+        {
+            overrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        }
+        animator.runtimeAnimatorController = overrideController;
+
+        currentAttackAnimSet = defaultAttackAnimSet;
+        combat.OnAttack += OnAttack;
+    }
+    
+    protected virtual void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomationAnimationSmoothTime, Time.deltaTime);
+
+        animator.SetBool("inCombat", combat.InCombat);
+    }
+
+    protected virtual void OnAttack()
+    {
+        animator.SetTrigger("attack");
+
+        // ПРОВЕРКА: Ако полето во Inspector е празно, прекини за да нема грешка
+        if (replaceableAttackAnim == null)
+        {
+            Debug.LogWarning("ГРЕШКА: Немаш ставено анимација во 'Replaceable Attack Anim' на објектот: " + gameObject.name);
+            return;
+        }
+
+        // Проверка дали низата со анимации има содржина
+        if (currentAttackAnimSet != null && currentAttackAnimSet.Length > 0)
+        {
+            int attackIndex = Random.Range(0, currentAttackAnimSet.Length);
+            
+            // Сега безбедно пристапуваме до .name бидејќи проверивме дека не е null
+            overrideController[replaceableAttackAnim.name] = currentAttackAnimSet[attackIndex];
+        }
+    }
+}
